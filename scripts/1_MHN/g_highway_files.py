65773984c2cca232f44e2380f35cc6e669903738
Cindy Cai <ccai@cmap.illinois.gov>
2025-08-11 16:13:52 -0500

    EOD 8/11 - editing HN module

---
+++ scripts/1_MHN/g_highway_files.py
@@ -0,0 +1,29 @@
+## g_highway_files.py
+## a translation of generate_highway_files.py
+## Author: npeterson
+## Translated + Updated by ccai (2025)
+
+from a_HN import HighwayNetwork
+
+import os
+import sys
+import argparse
+import math
+import time
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+
+    # generate highway files
+    HN = HighwayNetwork()
+    HN.generate_hwy_files()
+
+    end_time = time.time()
+    total_time = round(end_time - start_time)
+    minutes = math.floor(total_time / 60)
+    seconds = total_time % 60
+
+    print(f"{minutes}m {seconds}s to execute.")
+
+    print("Done")
