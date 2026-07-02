b5bf6d61d14dfc75a25286da90e7655e70b11bc5
Aaron Rumph <arumph@cmap.illinois.gov>
2026-07-02 13:36:59 -0500

    AR 07/02/2026: Fixed config MHN repo git check and config file writing

--- tests/config.py
+++ tests/config.py
@@ -98,6 +98,8 @@ def parse_arguments():
         help="Whether to time all tests by default",
     )
 
+    return parser.parse_args()
+
 
 def check_mhn_git_branch(mhn_repo_path: str):
     """
@@ -106,10 +108,13 @@ def check_mhn_git_branch(mhn_repo_path: str):
     """
     try:
         # check which branch currently on
-        check_branch_cmd = ["git", "branch", "-C", mhn_repo_path, "--show-current"]
-        current_branch = (
-            subprocess.check_output(check_branch_cmd).strip().decode("utf-8")
-        )
+        check_branch_cmd = f"git -C {str(mhn_repo_path)} branch --show-current"
+        current_branch = subprocess.run(
+            check_branch_cmd,
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout.strip()
 
         # CASE: not on main branch
         if current_branch != "main":
@@ -140,13 +145,23 @@ def write_test_config_file(
     Writes config options for testing to a python file called
     '_testing_config.py' to be used by tests to reduce number of env problems
     """
+    # docstring for config file (so good documentation)
+    _config_docstring = """'''
+This module contains user set config information for testing.
+Specifically, it provides the path for:
+SAS, ArcPy, the MHN Repo, the MHN GeoDatabase, and
+whether or not to time tests by default.
+'''
+"""
+
     # text to write to config (python) file so can be used in tests
-    config_text = f"""
-    MHN_REPO_PATH = {mhn_repo_path}
-    MHN_GDB_PATH = {mhn_gdb_path}
-    ARCPY_ENV_PATH = {arcpy_env_path}
-    SAS_PATH = {sas_path}
-    TIME_BY_DEFAULT = {time_by_default}
+    config_text = f"""{_config_docstring}
+
+MHN_REPO_PATH = r"{mhn_repo_path}"
+MHN_GDB_PATH = r"{mhn_gdb_path}"
+ARCPY_ENV_PATH = r"{arcpy_env_path}"
+SAS_PATH = r"{sas_path}"
+TIME_BY_DEFAULT = {time_by_default}
     """
     with open(TESTING_CONFIG_PATH, "w", encoding="utf-8") as config_file:
         config_file.write(config_text)
